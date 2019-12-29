7f2ed74a9971126220e3630d363c4779d345fab1
vinicius <nascimento.vinicius32@gmail.com>
2019-12-29 00:57:15 -0400

    criando usuarios

--- modulo2/readme.md
+++ modulo2/readme.md
@@ -1,2 +1,9 @@
-##code clean 
-yarn add eslint -D 
+##code clean
+yarn add eslint -D
+yarn add prettier eslint-config-prettier eslint-plugin-prettier -D
+
+#sequelize
+yarn add sequelize
+yarn add sequelize-cli -D
+#banco de dados dependencias
+yarn add pg pg-hstore
--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -1,9 +1,16 @@
-import { Router } from "express";
+import { Router } from 'express';
+
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "deu tudo certo campeão" });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'diego@rocketseat.com.br',
+    password_hash: '123456789',
+  });
+  return res.json(user);
 });
 
 export default routes;
