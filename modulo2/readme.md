f956fdbc5c4e3addb3c77bc8dd29efb81dfa12dd
vinicius <nascimento.vinicius32@gmail.com>
2019-12-30 18:22:05 -0400

    gerando autenticaçao com jwt token

--- modulo2/readme.md
+++ modulo2/readme.md
@@ -7,3 +7,8 @@ yarn add sequelize
 yarn add sequelize-cli -D
 #banco de dados dependencias
 yarn add pg pg-hstore
+#criptografia de senha
+yarn add bcryptjs
+
+#gerando o token com jsonwebtoken
+yarn add jsonwebtoken
--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -1,16 +1,13 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+// import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Diego Fernandes',
-    email: 'diego@rocketseat.com.br',
-    password_hash: '123456789',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
