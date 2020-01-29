a8644c8a279aa5b73eb2eeb7c5e6f4fa5de82af8
Vinicius Nascimento <nascimento.vinicius32@gmail.com>
2020-01-29 10:01:59 -0400

    update databasejs

--- modulo2/src/config/database.js
+++ modulo2/src/config/database.js
@@ -1,14 +1,12 @@
-require('dotenv/config');
+import 'dotenv/config';
 
-module.exports = {
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
+export const dialect = 'postgres';
+export const host = process.env.DB_HOST;
+export const username = process.env.DB_USER;
+export const password = process.env.DB_PASS;
+export const database = process.env.DB_NAME;
+export const define = {
+  timestamps: true,
+  underscored: true,
+  underscoredAll: true,
 };
