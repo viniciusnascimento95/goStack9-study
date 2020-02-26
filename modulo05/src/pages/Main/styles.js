bab290941f03f80038e9acc5c9ccd5eeca8959aa
Vinicius Nascimento <nascimento.vinicius32@gmail.com>
2020-02-26 03:30:06 -0500

    integration api github

--- modulo05/src/pages/Main/styles.js
+++ modulo05/src/pages/Main/styles.js
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
   max-width: 700px;
   background: #fff;
-  border-radius: 4px ;
+  border-radius: 4px;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
   padding: 30px;
   margin: 80px auto;
@@ -15,7 +15,7 @@ export const Container = styled.div`
     align-items: center;
 
     svg {
-      margin-right : 10px;
+      margin-right: 10px;
     }
   }
 `;
@@ -34,9 +34,20 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.button.attrs({
+const rotate = keyframes`
+from{
+  transform: rotate(0deg);
+}
+to {
+  transform: rotate(360deg);
+
+}
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
   type: 'submit',
-})`
+  disabled: props.loading,
+}))`
   background: #7159c1;
   border: 0;
   padding: 0 15px;
@@ -46,4 +57,17 @@ export const SubmitButton = styled.button.attrs({
   display: flex;
   justify-content: center;
   align-items: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+  ${props =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${rotate} 2s linear infinite;
+      }
+    `}
 `;
